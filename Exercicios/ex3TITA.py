212978229a1414978e40ab052fd27fc49ca6577f
vitormvrdev <vitormvr2001@gmail.com>
2023-10-27 19:51:00 +0100

    EX do teste

---
+++ Exercicios/ex3TITA.py
@@ -0,0 +1,9 @@
+num_int_1 = int(input("Número inteiro 1: \n"))
+num_int_2 = int(input("Número inteiro 2: \n"))
+num_real = float(input("Número real: \n"))
+
+produto = print((num_int_1 * 2) * (num_int_2 // 2))
+
+soma = print((num_int_1 * 3) + num_real)
+
+cubo = print(num_real ** 3)
