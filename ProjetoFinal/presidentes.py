e005f14344f4966ea198981212249cbda9177e49
vitormvrdev <124839908+vitormvrdev@users.noreply.github.com>
2024-01-20 19:37:44 +0000

    added more lists

--- ProjetoFinal/presidentes.py
+++ ProjetoFinal/presidentes.py
@@ -14,18 +14,37 @@ presidentes2 = [
     {"nome": "John Tyler", "ano_inicio": 1841, "ano_fim": 1845, "partido": "Whig"},
 ]
 presidentes3 = [
-    {"nome": "George Washington", "ano_inicio": 1789, "ano_fim": 1797, "partido": "Independente"},
-    {"nome": "John Adams", "ano_inicio": 1797, "ano_fim": 1801, "partido": "Federalista"},
+    {"nome": "James K. Polk", "ano_inicio": 1845, "ano_fim": 1849, "partido": "Democrata"},
+    {"nome": "Zachary Taylor", "ano_inicio": 1849, "ano_fim": 1850, "partido": "Whig"},
+    {"nome": "Millard Fillmore", "ano_inicio": 1850, "ano_fim": 1853, "partido": "Whig"},
+    {"nome": "Franklin Pierce", "ano_inicio": 1853, "ano_fim": 1857, "partido": "Democrata"},
+    {"nome": "James Buchanan", "ano_inicio": 1857, "ano_fim": 1861, "partido": "Democrata"},
 ]
 presidentes4 = [
-    {"nome": "George Washington", "ano_inicio": 1789, "ano_fim": 1797, "partido": "Independente"},
-    {"nome": "John Adams", "ano_inicio": 1797, "ano_fim": 1801, "partido": "Federalista"},
+    {"nome": "Abraham Lincoln", "ano_inicio": 1861, "ano_fim": 1865, "partido": "Republicano"},
+    {"nome": "Andrew Johnson", "ano_inicio": 1865, "ano_fim": 1869, "partido": "Democrata"},
+    {"nome": "Ulysses S. Grant", "ano_inicio": 1869, "ano_fim": 1877, "partido": "Republicano"},
+    {"nome": "Rutherford B. Hayes", "ano_inicio": 1877, "ano_fim": 1881, "partido": "Republicano"},
+    {"nome": "James A. Garfield", "ano_inicio": 1881, "ano_fim": 1881, "partido": "Republicano"},
 ]
 presidentes5 = [
-    {"nome": "George Washington", "ano_inicio": 1789, "ano_fim": 1797, "partido": "Independente"},
-    {"nome": "John Adams", "ano_inicio": 1797, "ano_fim": 1801, "partido": "Federalista"},
+    {"nome": "Chester A. Arthur", "ano_inicio": 1881, "ano_fim": 1885, "partido": "Republicano"},
+    {"nome": "Grover Cleveland", "ano_inicio": 1885, "ano_fim": 1889, "partido": "Democrata"},
+    {"nome": "Benjamin Harrison", "ano_inicio": 1889, "ano_fim": 1893, "partido": "Republicano"},
+    {"nome": "Grover Cleveland", "ano_inicio": 1893, "ano_fim": 1897, "partido": "Democrata"},
+    {"nome": "William McKinley", "ano_inicio": 1897, "ano_fim": 1901, "partido": "Republicano"},
 ]
 presidentes6 = [
-    {"nome": "George Washington", "ano_inicio": 1789, "ano_fim": 1797, "partido": "Independente"},
-    {"nome": "John Adams", "ano_inicio": 1797, "ano_fim": 1801, "partido": "Federalista"},
+    {"nome": "Theodore Roosevelt", "ano_inicio": 1901, "ano_fim": 1909, "partido": "Republicano"},
+    {"nome": "William Howard Taft", "ano_inicio": 1909, "ano_fim": 1913, "partido": "Republicano"},
+    {"nome": "Woodrow Wilson", "ano_inicio": 1913, "ano_fim": 1921, "partido": "Democrata"},
+    {"nome": "Warren G. Harding", "ano_inicio": 1921, "ano_fim": 1923, "partido": "Republicano"},
+    {"nome": "Calvin Coolidge", "ano_inicio": 1923, "ano_fim": 1929, "partido": "Republicano"},
+]
+presidentes7 = [
+    {"nome": "George H. W. Bush", "ano_inicio": 1989, "ano_fim": 1993, "partido": "Republicano"},
+    {"nome": "Bill Clinton", "ano_inicio": 1993, "ano_fim": 2001, "partido": "Democrata"},
+    {"nome": "George W. Bush", "ano_inicio": 2001, "ano_fim": 2009, "partido": "Republicano"},
+    {"nome": "Barack Obama", "ano_inicio": 2009, "ano_fim": 2017, "partido": "Democrata"},
+    {"nome": "Donald Trump", "ano_inicio": 2017, "ano_fim": 2021, "partido": "Republicano"},
 ]
