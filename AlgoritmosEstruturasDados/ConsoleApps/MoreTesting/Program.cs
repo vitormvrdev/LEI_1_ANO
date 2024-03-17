53eda6163134b18b2832615fc916f3d619b855fa
vitormvrdev <124839908+vitormvrdev@users.noreply.github.com>
2024-03-17 17:45:39 +0000

    commit

--- AlgoritmosEstruturasDados/ConsoleApps/MoreTesting/Program.cs
+++ AlgoritmosEstruturasDados/ConsoleApps/MoreTesting/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace MoreTesting
 {
-    internal class Program
+    internal class TRYPARSE
     {
         static void Main(string[] args)
         {
+            string numberAsString = "128";
 
+            bool canParse = int.TryParse(numberAsString, out int parsedValue);
+
+            // returns true if the parsing is successful
+
+            if (canParse)
+            {
+                Console.WriteLine($"Parsing successful - number is {parsedValue}");
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed.");
+            }
         }
     }
 }
--- AlgoritmosEstruturasDados/ConsoleApps/TestingAroundWithCsharp/obj/Debug/net8.0/TestingAroundWithCsharp.AssemblyInfo.cs
+++ AlgoritmosEstruturasDados/ConsoleApps/TestingAroundWithCsharp/obj/Debug/net8.0/TestingAroundWithCsharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestingAroundWithCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3aec30c3b87974d29e53c4bfa396773d092b92d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74fd28e3a2798d724f231037e4de8c0c4352f21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestingAroundWithCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestingAroundWithCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
