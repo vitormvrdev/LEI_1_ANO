53f258e6e11b8d1b5529df059e641a149bec9c17
vitormvrdev <vitormvr2001@gmail.com>
2024-10-18 19:42:54 +0100

    commit

--- 1_ano/AlgoritmosEstruturasDados/ConsoleApps/LoginForm/obj/Debug/net8.0-windows/LoginForm.AssemblyInfo.cs
+++ 1_ano/AlgoritmosEstruturasDados/ConsoleApps/LoginForm/obj/Debug/net8.0-windows/LoginForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4adf3c83cf5cbd71fcad1876eade6a163f9efb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc8c80e71baa11c2c63cbc31981c7d150ea8a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
